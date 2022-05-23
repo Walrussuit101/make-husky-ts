a148a093a3b2f5806c6450e52324d95e6a90bbc9
Walrussuit101 <tjefferson@ycp.edu>
2022-05-22 22:58:42 -0400

    install deps / setup husky & lint-staged

--- index.ts
+++ index.ts
@@ -22,14 +22,54 @@ const createProjectDirectory = (projectName: string): string => {
     return projectDirectory;
 };
 
+// Install dependencies in the project directory
+const installDeps = (projectDirectory: string): void => {
+    const DEV_DEPS = [
+        "typescript",
+        "ts-node",
+        "@types/node",
+        "husky",
+        "prettier",
+        "lint-staged"
+    ];
+
+    let installCmd = "npm install --save-dev";
+
+    DEV_DEPS.forEach((DEP) => {
+        installCmd += ` ${DEP}`;
+    });
+
+    execSync(installCmd, { cwd: projectDirectory });
+};
+
 const mainProc = (projectName: string) => {
-    // - install deps
-    // - add package file keys (prettier / lint-staged), add prepare script ('husky install'), add start script ('ts-node src/index.ts')
-    // - add husky precommit that calls 'npx lint-staged'
     // - make src/index.ts file that console.log(<project_name>)
 
     const projectDirectory = createProjectDirectory(projectName);
-    execSync("npm init -y", { cwd: projectDirectory });
+    execSync("git init && npm init -y", { cwd: projectDirectory }); // setup git repo / npm project
+    execSync("npm set-script prepare 'husky install' && npm run prepare", {
+        cwd: projectDirectory
+    }); // install husky
+    execSync("npm set-script start 'ts-node src/index.ts'", {
+        cwd: projectDirectory
+    }); // add start command
+
+    installDeps(projectDirectory);
+    execSync(
+        "npm pkg set lint-staged.**/*='prettier --write --ignore-unknown'",
+        { cwd: projectDirectory }
+    ); // add lint-staged key
+    execSync("npm pkg set prettier.tabWidth=4 --json", {
+        cwd: projectDirectory
+    }); // add prettier config
+    execSync("npm pkg set prettier.endOfLine=lf", { cwd: projectDirectory });
+    execSync("npm pkg set prettier.trailingComma=none", {
+        cwd: projectDirectory
+    });
+
+    execSync("npx husky add .husky/pre-commit 'npx lint-staged'", {
+        cwd: projectDirectory
+    }); // add husky precommit hook
 
     console.log(projectName);
 };
